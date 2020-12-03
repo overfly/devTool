1c4479dba067fc317e7643c54b9029602064e7d3
Zhang <edward.zhang@sap.com>
2020-12-03 15:07:02 +0800

    Initial commit

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+set http_proxy=http://proxy.wdf.sap.corp:8080
+set https_proxy=http://proxy.wdf.sap.corp:8080
+rem pip3 install venv
+IF NOT EXIST env (
+	echo "Install virtual environment."
+	python -m venv env
+	call env\Scripts\activate.bat
+) else (
+	echo "Skipped, virtualenv has been installed."
+)
+env\Scripts\pip install -r requirements.txt
